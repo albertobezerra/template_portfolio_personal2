0bb8fc3257968e9b6a1470b5fcd62751ecbfd1f2
Alberto Bezerra <albertofbezerra@gmail.com>
2019-03-04 10:07:53 -0300

    Initial commit

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php 
+
+function add_estilos_e_scripts() {
+  wp_enqueue_script( 'script', get_template_directory_uri() . '/assets/vendor/jquery/jquery.min.js', array ( 'jquery' ), 1.1, true);
+  wp_enqueue_script( 'script2', get_template_directory_uri() . '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js', array ( 'jquery' ), 1.1, true);
+  wp_enqueue_script( 'script3', get_template_directory_uri() . '/assets/vendor/jquery-easing/jquery.easing.min.js', array ( 'jquery' ), 1.1, true);
+  wp_enqueue_script( 'script4', get_template_directory_uri() . '/assets/js/resume.min.js', array ( 'jquery' ), 1.1, true);
+}
+add_action( 'wp_enqueue_scripts', 'add_estilos_e_scripts' );
+
+
+
+ ?>
---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title><?php wp_title('-',true,'right'); bloginfo(); ?></title>
+
+  <link rel="shortcut icon" href="<?php bloginfo( 'template_directory' ); ?>/assets/images/favicon.png" type="image/x-icon">
+
+  <?php wp_head(); ?>
+
+  <!-- Bootstrap core CSS -->
+  <link href="<?php bloginfo('template_url') ?>/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
+  <link href="<?php bloginfo('template_url') ?>/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="<?php bloginfo('template_url') ?>/assets/css/resume.min.css" rel="stylesheet">
+
+</head>
+
+<body>
